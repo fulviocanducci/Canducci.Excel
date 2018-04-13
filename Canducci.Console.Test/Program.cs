e0d5f8a70d6b3bb77ef93f7e84b2b8babea7f68a
fulviocanducci <fulviocanducci@hotmail.com>
2018-04-13 14:39:34 -0300

    Console Test

---
+++ Canducci.Console.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Canducci.Excel;
+namespace Canducci.Console.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inteiros = new int[5]
+            {
+                1,2,3,4,5
+            };
+            inteiros.ToExcelSaveAs("Inteiros.xlsx");
+
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
