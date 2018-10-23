324ca68faba0f5f4daa57acd71c305f093de2151
Fulvio <fulviocanducci@hotmail.com>
2018-10-23 11:14:33 -0300

    Test

---
+++ Canducci.Console.Test/People.cs
@@ -0,0 +1,10 @@
+﻿namespace Canducci.Console.Test
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; } = 10;
+        public System.DateTime DateCreated { get; set; } = System.DateTime.Now;
+    }
+}
--- Canducci.Console.Test/Program.cs
+++ Canducci.Console.Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Canducci.Excel;
 namespace Canducci.Console.Test
 {
@@ -11,9 +12,21 @@ namespace Canducci.Console.Test
             //    1,2,3,4,5
             //};
             //var headerCollection = HeaderCollection.Create();
-            //headerCollection.Add(Header.Create("Numeros",1));
+            //headerCollection.Add(Header.Create("Numeros", 1));
             //inteiros.ToExcelSaveAs("Inteiros.xlsx", headerCollection);
 
+            IList<People> peoples = new List<People>();
+            peoples.Add(new People
+            {
+                Id = 1,
+                Name = "Name 1"
+            });
+            peoples.Add(new People
+            {
+                Id = 2,
+                Name = "Name 2"
+            });
+            peoples.ToExcelSaveAs("Peoples.xlsx");
             var a = ContentTypeExcel.Xls.ToValue();
             var b = ContentTypeExcel.Xlsx.ToValue();
             
